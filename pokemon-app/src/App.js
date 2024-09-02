294328d903949ecd8ad20d9d73ef66020cd121fa
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-09-02 17:24:35 +0900

    add: ポケモンデータを取得できるように

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -1,7 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import { getAllPokemon } from './utils/pokemon';
 
 function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon"
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      // すべてのポケモンのデータを取得
+      let res = await getAllPokemon(initialURL);
+      console.log(res);
+    }
+    fetchPokemonData();
+  }, []);
   return (
     <div className="App"></div>
   );
