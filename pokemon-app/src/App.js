0f4d47fd9917bd8f421ceafe19df5ef9fdecb3c8
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-09-02 17:52:03 +0900

    add: 個別のポケモンデータを所得できるように

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -1,21 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
-import { getAllPokemon } from './utils/pokemon';
+import { useEffect, useState } from 'react';
+import { getAllPokemon, getPokemon } from './utils/pokemon';
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon"
+  const [loading, setLoding] = useState(true);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       // すべてのポケモンのデータを取得
       let res = await getAllPokemon(initialURL);
-      console.log(res);
+      loadPokemon(res.results);
+      setLoding(false);
     }
     fetchPokemonData();
   }, []);
+
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    console.log(_pokemonData)
+  }
+
+  
+
   return (
-    <div className="App"></div>
+    <div className="App">
+      {loading ? (
+        <h1>ロード中。。。</h1>
+      ) : (
+        <h1>ポケモンデータを取得</h1>
+      )
+      }
+    </div>
   );
 }
 
